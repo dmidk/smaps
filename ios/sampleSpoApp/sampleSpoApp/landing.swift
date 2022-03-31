adc379ff76c5f826f83c67cc638fee87f853a9e6
Kasper Hintz <kah@dmi.dk>
2022-03-31 17:54:08 +0200

    clean code in sample app

--- ios/sampleSpoApp/sampleSpoApp/landing.swift
+++ ios/sampleSpoApp/sampleSpoApp/landing.swift
@@ -7,33 +7,3 @@
 
 import SwiftUI
 import smaps
-
-//class ExternalModel: ObservableObject {
-//    @Published var textToUpdate: String = "Update me!"
-//    func registerRequest() {
-//        // other functionality
-//        textToUpdate = "I've been updated!"
-//    }
-//}
-//
-//struct updateLanding: View {
-//    @ObservedObject var viewModel: ExternalModel
-//
-//    var body: some View {
-//            VStack {
-//                Button(action: {
-//                    self.viewModel.registerRequest()
-//                }) {
-//                    Text("ClickMe")
-//                }
-//                Text(self.viewModel.textToUpdate)
-//            }
-//
-//        }
-//}
-//
-//struct landing_Previews: PreviewProvider {
-//    static var previews: some View {
-//        updateLanding(viewModel: ExternalModel())
-//        }
-//}

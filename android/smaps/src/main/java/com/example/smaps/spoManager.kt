f34fa3476fcf092514a71612946796a390050ab9
Kasper Hintz <kah@dmi.dk>
2022-05-23 16:59:58 +0200

    location is retrieved succesfully

--- android/smaps/src/main/java/com/example/smaps/spoManager.kt
+++ android/smaps/src/main/java/com/example/smaps/spoManager.kt
@@ -15,125 +15,127 @@ import android.hardware.SensorEventListener
 import android.os.Bundle
 import android.app.Activity
 
-interface LocationCallback: AirLocation.Callback {
-    override fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum)  {
-        onFailure()
-    }
-    fun onFailure()
-}
-
-class SpoManager(private val activity: AppCompatActivity) : Activity(), SensorEventListener {
-
-    private var mSensorManager: SensorManager? = null
-    private var mPressure: Sensor? = null
-    var lat: Double? = null
-    var lon: Double? = null
-    var alt: Double? = null
-    var ps: Float? = null
-
-    private var airLocation = AirLocation(this, object : AirLocation.Callback {
-
-        override fun onSuccess(locations: ArrayList<Location>) {
-            // do something
-            // the entire track is sent in locations
-        }
-
-        override fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum) {
-            // do something
-            // the reason for failure is given in locationFailedEnum
-        }
-
-    })
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //airLocation.start() // CALL .start() WHEN YOU ARE READY TO RECEIVE LOCATION UPDATES
-        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mPressure = mSensorManager!!.getDefaultSensor(Sensor.TYPE_PRESSURE)
-    }
+//interface LocationCallback: AirLocation.Callback {
+//    override fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum)  {
+//        onFailure()
+//    }
+//    fun onFailure()
+//}
+//
+//class SpoManager(private val activity: AppCompatActivity) : Activity(), SensorEventListener {
+//
+//    private var mSensorManager: SensorManager? = null
+//    private var mPressure: Sensor? = null
+//    var lat: Double? = null
+//    var lon: Double? = null
+//    var alt: Double? = null
+//    var ps: Float? = null
+//
+//    private var airLocation = AirLocation(this, object : AirLocation.Callback {
+//
+//        override fun onSuccess(locations: ArrayList<Location>) {
+//            // do something
+//            // the entire track is sent in locations
+//        }
+//
+//        override fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum) {
+//            // do something
+//            // the reason for failure is given in locationFailedEnum
+//        }
+//
+//    })
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        //airLocation.start() // CALL .start() WHEN YOU ARE READY TO RECEIVE LOCATION UPDATES
+//        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        mPressure = mSensorManager!!.getDefaultSensor(Sensor.TYPE_PRESSURE)
+//    }
+//
+//    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        airLocation.onActivityResult(requestCode, resultCode, data) // ADD THIS LINE INSIDE onActivityResult
+//    }
+//
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        airLocation.onRequestPermissionsResult(requestCode, permissions, grantResults) // ADD THIS LINE INSIDE onRequestPermissionResult
+//    }
+//
+//    override fun onSensorChanged(event: SensorEvent?) {
+//        if (event != null) {
+//            ps = event.values[0]
+//            Log.e("New Pressure", "$ps")
+//        }
+//        // Do something with this sensor data.
+//    }
+//
+//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+//        // Do something here if sensor accuracy changes.
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun onResume() {
+//        // Register a listener for the sensor.
+//        super.onResume()
+//        mSensorManager!!.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL)
+//    }
+//
+//    override fun onPause() {
+//        // Be sure to unregister the sensor when the activity pauses.
+//        super.onPause()
+//        mSensorManager!!.unregisterListener(this)
+//    }
+//
+//    fun startLocation(callback: LocationCallback) {
+//        airLocation = AirLocation(activity,
+//            callback = callback)
+//    }
+//
+//    fun start(){
+//
+//        startLocation(object: LocationCallback {
+//            override fun onFailure() {
+//                Log.e("failed", "Location Fetch failed for some reason")
+//            }
+//
+//
+//            override fun onSuccess(location: ArrayList<Location>) {
+//                Log.e("New location", "${location[0].latitude}  --- ${location[0].longitude}")
+//                lat = location[0].latitude
+//                lon = location[0].longitude
+//                alt = location[0].altitude
+//            }
+//        })
+//
+//
+//        var sensorEventListener: SensorEventListener = object : SensorEventListener {
+//            override fun onSensorChanged(event: SensorEvent) {
+//                val values = event.values
+//                val ps = String.format("%.3f hPa", values[0])
+//            }
+//
+//            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+//                Log.d("MY_APP", "$sensor - $accuracy")
+//            }
+//        }
+//
+//        Log.e("lat", "${lat}") // Not necesarrily initialised after lat have a value
+//
+//        Log.e("ps", "${ps}")
+//        //Log.e("sensor", "${sensorEventListener}")
+//
+//    }
+//}
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        airLocation.onActivityResult(requestCode, resultCode, data) // ADD THIS LINE INSIDE onActivityResult
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        airLocation.onRequestPermissionsResult(requestCode, permissions, grantResults) // ADD THIS LINE INSIDE onRequestPermissionResult
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            ps = event.values[0]
-            Log.e("New Pressure", "$ps")
-        }
-        // Do something with this sensor data.
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        // Do something here if sensor accuracy changes.
-        TODO("Not yet implemented")
-    }
-
-    override fun onResume() {
-        // Register a listener for the sensor.
-        super.onResume()
-        mSensorManager!!.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL)
-    }
-
-    override fun onPause() {
-        // Be sure to unregister the sensor when the activity pauses.
-        super.onPause()
-        mSensorManager!!.unregisterListener(this)
-    }
-
-    fun startLocation(callback: LocationCallback) {
-        airLocation = AirLocation(activity,
-            callback = callback)
-    }
-
-    fun start(){
-
-        startLocation(object: LocationCallback {
-            override fun onFailure() {
-                Log.e("failed", "Location Fetch failed for some reason")
-            }
-
-
-            override fun onSuccess(location: ArrayList<Location>) {
-                Log.e("New location", "${location[0].latitude}  --- ${location[0].longitude}")
-                lat = location[0].latitude
-                lon = location[0].longitude
-                alt = location[0].altitude
-            }
-        })
-
-
-        var sensorEventListener: SensorEventListener = object : SensorEventListener {
-            override fun onSensorChanged(event: SensorEvent) {
-                val values = event.values
-                val ps = String.format("%.3f hPa", values[0])
-            }
-
-            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-                Log.d("MY_APP", "$sensor - $accuracy")
-            }
-        }
-
-        Log.e("blabla", "${lat}") // Not necesarrily initialised after lat have a value
-
-        Log.e("ppppp", "${ps}")
-        Log.e("ppppp", "${sensorEventListener}")
-
-    }
 
 
 
-}
-/*
-interface LocationCallback: AirLocation.Callback { //LocationCallback: AirLocation.Callbacks
-    override fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum)  {
+interface LocationCallback: AirLocation.Callbacks { //LocationCallback: AirLocation.Callbacks
+    fun onFailure(locationFailedEnum: AirLocation.LocationFailedEnum)  {
         onFailure()
     }
     fun onFailure() // onFailed was renamed to onFailure
@@ -195,7 +197,7 @@ class SpoManager(private val activity: AppCompatActivity) : Activity(), SensorEv
         airLocation = AirLocation(activity,
             shouldWeRequestPermissions = true,
             shouldWeRequestOptimization = true,
-            callback = callback) //callbacks = callback
+            callbacks = callback) //callbacks = callback
     }
 
     //fun startBarometer(callback: PressureCallback){
@@ -210,8 +212,12 @@ class SpoManager(private val activity: AppCompatActivity) : Activity(), SensorEv
                 Log.e("failed", "Location Fetch failed for some reason")
             }
 
+            override fun onFailed(locationFailedEnum: AirLocation.LocationFailedEnum) {
+                TODO("Not yet implemented")
+            }
+
             override fun onSuccess(location: Location) {
-                Log.e("New location", "${location.latitude}  --- ${location.longitude}")
+                Log.e("New location", "${location.latitude}, ${location.longitude}, ${location.altitude}")
                 lat = location.latitude
                 lon = location.longitude
                 alt = location.altitude
@@ -230,11 +236,9 @@ class SpoManager(private val activity: AppCompatActivity) : Activity(), SensorEv
             }
         }
 
-        Log.e("blabla", "${lat}") // Not necesarrily initialised after lat have a value
-
-        Log.e("ppppp", "${ps}")
-        Log.e("ppppp", "${sensorEventListener}")
+        //Log.e("blabla", "${lat}") // Not necesarrily initialised after lat have a value
+        //Log.e("ppppp", "${ps}")
+        //Log.e("ppppp", "${sensorEventListener}")
 
     }
 }
-*/
